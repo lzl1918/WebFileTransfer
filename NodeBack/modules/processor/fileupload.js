642c0399afc1ad8e27667f474436ef05e4147aac
Hake <lzl1918@outlook.com>
2017-04-01 14:13:59 +0800

    file uploading for node.js

--- NodeBack/modules/processor/fileupload.js
+++ NodeBack/modules/processor/fileupload.js
@@ -6,14 +6,85 @@ var startswith = function (thisstr, str) {
     return true;
 };
 
-var fileupload = function (fileroot, url) {
+var path = require('path');
+var fs = require('fs');
+var multiparty = require('multiparty');
+
+var varify_path = function (parampath, fullpath, fileroot) {
+    if (!startswith(fullpath, fileroot)) return false;
+    return true;
+};
+var cleanfile = function (fullpath) {
+    fs.unlinkSync(fullpath);
+};
+var cleanfiles = function (files) {
+    for (var i = 0; i < files.length; i++) {
+        cleanfile(files.path);
+    }
+};
+var fileupload = function (fileroot, tmpdir, url) {
     this.fileroot = path.resolve(fileroot);
+    this.tmpdir = path.resolve(tmpdir);
     this.url = url;
 };
+
 var fileupload_process = function (req, res) {
     if (req.method !== 'POST') return false;
     if (startswith(req.url, this.url) === false) return false;
+    var rootpath = this.fileroot;
 
+    var form = new multiparty.Form({
+        uploadDir: this.tmpdir
+    });
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            res.writeHeader(503);
+            res.end();
+            return;
+        }
+        if (fields && fields['filePath'] && files['files[]']) {
+            var filepath = path.join(rootpath, fields['filePath'][0]);
+            if (!startswith(filepath, rootpath)) {
+                cleanfiles(files['files[]']);
+                res.writeHeader(403);
+                res.end();
+                return;
+            }
+            var uploadfiles = files['files[]'];
+            var fullpath;
+            if (uploadfiles.length == 1) {
+                var filename = fields['fileName'][0];
+                if (!(filename && filename.length > 0)) filename = uploadfiles[0].originalFilename;
+                fullpath = path.join(filepath, filename);
+                fs.exists(fullpath, function (ext) {
+                    if (ext) { cleanfiles(uploadfiles); res.writeHeader(200); res.end(); return; }
+                    fs.renameSync(uploadfiles[0].path, fullpath);
+                    res.writeHeader(200);
+                    res.end();
+                });
+            }
+            else {
+                var file;
+                for (var i = 0; i < uploadfiles.length; i++) {
+                    file = uploadfiles[i];
+                    fullpath = path.join(filepath, file.originalFilename);
+                    if (fs.existsSync(fullpath)) {
+                        cleanfile(file.path);
+                    }
+                    else {
+                        fs.renameSync(file.path, fullpath);
+                    }
+                }
+                res.writeHeader(200);
+                res.end();
+            }
+        }
+        else {
+            res.writeHeader(403);
+            res.end();
+            return;
+        }
+    });
     return true;
 };
 
--- NodeBack/server.js
+++ NodeBack/server.js
@@ -3,18 +3,25 @@
 var config = require('./modules/configreader').read();
 var pipeline = require('./modules/pipeline').create(config);
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
 
 var filequery = require('./modules/processor/filequery');
 var filedownload = require('./modules/processor/filedownload');
-//var fileupload = require('./modules/processor/fileupload');
+var fileupload = require('./modules/processor/fileupload');
 var createdir = require('./modules/processor/createdir');
 
 pipeline.use_default();
 pipeline.use_static();
 pipeline.add(new filequery.filequery(config.file_root, '/api/filedata/data'));
 pipeline.add(new filedownload.filedownload(config.file_root, '/api/filedata/down'));
-//pipeline.add(new fileupload.fileupload(config.file_root, '/api/filedata/up'));
+
 pipeline.add(new createdir.createdir(config.file_root, '/api/filedata/createdir'));
+
+var uploadtmp = path.resolve(config.file_root);
+uploadtmp = path.join(uploadtmp, "tmp");
+if (!fs.existsSync(uploadtmp)) fs.mkdirSync(uploadtmp);
+pipeline.add(new fileupload.fileupload(config.file_root, uploadtmp, '/api/filedata/up'));
 
 http.createServer(function (req, res) {
     // go through the pipe line 
